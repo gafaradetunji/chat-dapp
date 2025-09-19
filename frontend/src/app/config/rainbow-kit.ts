7a6a68f290caf7ed6fa02072c50b821f41054ef4
gafaradetunji <gafaradetunji471@gmail.com>
2025-09-19 11:04:31 +0100

    updated app kit name

--- frontend/src/app/config/rainbow-kit.ts
+++ frontend/src/app/config/rainbow-kit.ts
@@ -4,7 +4,7 @@ import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { sepolia } from "wagmi/chains";
 
 export const config = getDefaultConfig({
-  appName: "Chat dApp",
+  appName: "chat App",
   projectId: process.env["NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID"]!,
   chains: [sepolia],
 });

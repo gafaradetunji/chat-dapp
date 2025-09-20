ca942a231852fef3b901383811d10c09b4e81c99
gafaradetunji <gafaradetunji471@gmail.com>
2025-09-20 11:18:13 +0100

    chat dapp

--- frontend/src/common/lib/client.ts
+++ frontend/src/common/lib/client.ts
@@ -1,15 +1,15 @@
 import { createPublicClient, fallback, http } from 'viem'
 import { sepolia } from 'viem/chains'
 import { chatAbi, multicallAbi } from './abi';
- 
-export const publicClient = createPublicClient({ 
+
+export const publicClient = createPublicClient({
   chain: sepolia,
-  // transport: http(`${process.env["NEXT_PUBLIC_SEPOLIA_RPC_URL"]}`)
-  transport: fallback([
-    http('https://rpc.sepolia.org'),
-    http(`${process.env["NEXT_PUBLIC_SEPOLIA_RPC_URL"]}`),
-    http('https://rpc.ankr.com/eth_sepolia')
-  ])
+  transport: http(`${process.env["NEXT_PUBLIC_SEPOLIA_RPC_URL"]}`)
+  // transport: fallback([
+  //   http('https://rpc.sepolia.org'),
+  //   http(`${process.env["NEXT_PUBLIC_SEPOLIA_RPC_URL"]}`),
+  //   http('https://rpc.ankr.com/eth_sepolia')
+  // ])
 })
 
 export const multicalCaontract = {
